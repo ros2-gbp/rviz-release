1d2e53a0074491281efd9177215c94ce2e959702
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-01-15 10:23:01 +0100

    Imported upstream version '14.4.1' of 'upstream'

--- rviz_default_plugins/src/rviz_default_plugins/displays/image/ros_image_texture.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/displays/image/ros_image_texture.cpp
@@ -1,31 +1,32 @@
-/*
- * Copyright (c) 2009, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (c) 2009, Willow Garage, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 
 #include "rviz_default_plugins/displays/image/ros_image_texture.hpp"
 
@@ -38,6 +39,7 @@
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <vector>
 #include <utility>
 
@@ -189,6 +191,32 @@ bool ROSImageTexture::update()
   return true;
 }
 
+// Considering frequent execution, use inline
+static inline std::tuple<uint8_t, uint8_t, uint8_t> pixelYUVToRGB(int y, int u, int v)
+{
+  int r = 0;
+  int b = 0;
+  int g = 0;
+
+  // Values generated based on this formula
+  // for converting YUV to RGB
+  // R = Y + 1.403V'
+  // G = Y + 0.344U' - 0.714V'
+  // B = Y + 1.770U'
+
+  v -= 128;
+  u -= 128;
+
+  r = y + (1403 * v) / 1000;
+  g = y + (344 * u - 714 * v) / 1000;
+  b = y + (1770 * u) / 1000;
+
+  // pixel value must fit in a uint8_t
+  return {
+    std::clamp(r, 0, 255), std::clamp(g, 0, 255), std::clamp(b, 0, 255)
+  };
+}
+
 struct yuyv
 {
   uint8_t y0;
@@ -205,8 +233,8 @@ struct uyvy
   uint8_t y1;
 };
 
-// Function converts src_img from yuv422 format to rgb
-static void imageConvertYUV422ToRGB(
+// Function converts src_img from UYVY format to rgb
+static void imageConvertUYVYToRGB(
   uint8_t * dst_img, uint8_t * src_img,
   int dst_start_row, int dst_end_row,
   int dst_num_cols, uint32_t stride_in_bytes)
@@ -215,12 +243,6 @@ static void imageConvertYUV422ToRGB(
   int final_u = 0;
   int final_y1 = 0;
   int final_v = 0;
-  int r1 = 0;
-  int b1 = 0;
-  int g1 = 0;
-  int r2 = 0;
-  int b2 = 0;
-  int g2 = 0;
 
   uint32_t stride_in_pixels = stride_in_bytes / 4;
 
@@ -236,37 +258,15 @@ static void imageConvertYUV422ToRGB(
       final_y1 = pixel->y1;
       final_v = pixel->v;
 
-      // Values generated based on this formula
-      // for converting YUV to RGB
-      // R = Y + 1.403V'
-      // G = Y + 0.344U' - 0.714V'
-      // B = Y + 1.770U'
-
-      final_v -= 128;
-      final_u -= 128;
-
-      r1 = final_y0 + (1403 * final_v) / 1000;
-      g1 = final_y0 + (344 * final_u - 714 * final_v) / 1000;
-      b1 = final_y0 + (1770 * final_u) / 1000;
-
-      r2 = final_y1 + (1403 * final_v) / 1000;
-      g2 = final_y1 + (344 * final_u - 714 * final_v) / 1000;
-      b2 = final_y1 + (1770 * final_u) / 1000;
-
-      // pixel value must fit in a uint8_t
-      dst_img[0] = ((r1 & 0xFFFFFF00) == 0) ? r1 : (r1 < 0) ? 0 : 0xFF;
-      dst_img[1] = ((g1 & 0xFFFFFF00) == 0) ? g1 : (g1 < 0) ? 0 : 0xFF;
-      dst_img[2] = ((b1 & 0xFFFFFF00) == 0) ? b1 : (b1 < 0) ? 0 : 0xFF;
-      dst_img[3] = ((r2 & 0xFFFFFF00) == 0) ? r2 : (r2 < 0) ? 0 : 0xFF;
-      dst_img[4] = ((g2 & 0xFFFFFF00) == 0) ? g2 : (g2 < 0) ? 0 : 0xFF;
-      dst_img[5] = ((b2 & 0xFFFFFF00) == 0) ? b2 : (b2 < 0) ? 0 : 0xFF;
+      std::tie(dst_img[0], dst_img[1], dst_img[2]) = pixelYUVToRGB(final_y0, final_u, final_v);
+      std::tie(dst_img[3], dst_img[4], dst_img[5]) = pixelYUVToRGB(final_y1, final_u, final_v);
       dst_img += 6;
     }
   }
 }
 
-// Function converts src_img from yuv422_yuy2 format to rgb
-static void imageConvertYUV422_YUY2ToRGB(
+// Function converts src_img from YUYV format to rgb
+static void imageConvertYUYVToRGB(
   uint8_t * dst_img, uint8_t * src_img,
   int dst_start_row, int dst_end_row,
   int dst_num_cols, uint32_t stride_in_bytes)
@@ -275,12 +275,6 @@ static void imageConvertYUV422_YUY2ToRGB(
   int final_u = 0;
   int final_y1 = 0;
   int final_v = 0;
-  int r1 = 0;
-  int b1 = 0;
-  int g1 = 0;
-  int r2 = 0;
-  int b2 = 0;
-  int g2 = 0;
 
   uint32_t stride_in_pixels = stride_in_bytes / 4;
 
@@ -296,30 +290,8 @@ static void imageConvertYUV422_YUY2ToRGB(
       final_y1 = pixel->y1;
       final_v = pixel->v;
 
-      // Values generated based on this formula
-      // for converting YUV to RGB
-      // R = Y + 1.403V'
-      // G = Y + 0.344U' - 0.714V'
-      // B = Y + 1.770U'
-
-      final_v -= 128;
-      final_u -= 128;
-
-      r1 = final_y0 + (1403 * final_v) / 1000;
-      g1 = final_y0 + (344 * final_u - 714 * final_v) / 1000;
-      b1 = final_y0 + (1770 * final_u) / 1000;
-
-      r2 = final_y1 + (1403 * final_v) / 1000;
-      g2 = final_y1 + (344 * final_u - 714 * final_v) / 1000;
-      b2 = final_y1 + (1770 * final_u) / 1000;
-
-      // pixel value must fit in a uint8_t
-      dst_img[0] = ((r1 & 0xFFFFFF00) == 0) ? r1 : (r1 < 0) ? 0 : 0xFF;
-      dst_img[1] = ((g1 & 0xFFFFFF00) == 0) ? g1 : (g1 < 0) ? 0 : 0xFF;
-      dst_img[2] = ((b1 & 0xFFFFFF00) == 0) ? b1 : (b1 < 0) ? 0 : 0xFF;
-      dst_img[3] = ((r2 & 0xFFFFFF00) == 0) ? r2 : (r2 < 0) ? 0 : 0xFF;
-      dst_img[4] = ((g2 & 0xFFFFFF00) == 0) ? g2 : (g2 < 0) ? 0 : 0xFF;
-      dst_img[5] = ((b2 & 0xFFFFFF00) == 0) ? b2 : (b2 < 0) ? 0 : 0xFF;
+      std::tie(dst_img[0], dst_img[1], dst_img[2]) = pixelYUVToRGB(final_y0, final_u, final_v);
+      std::tie(dst_img[3], dst_img[4], dst_img[5]) = pixelYUVToRGB(final_y1, final_u, final_v);
       dst_img += 6;
     }
   }
@@ -410,13 +382,13 @@ ROSImageTexture::convertTo8bit(const uint8_t * data_ptr, size_t data_size_in_byt
 }
 
 ImageData
-ROSImageTexture::convertYUV422ToRGBData(const uint8_t * data_ptr, size_t data_size_in_bytes)
+ROSImageTexture::convertUYVYToRGBData(const uint8_t * data_ptr, size_t data_size_in_bytes)
 {
   size_t new_size_in_bytes = data_size_in_bytes * 3 / 2;
 
   uint8_t * new_data = new uint8_t[new_size_in_bytes];
 
-  imageConvertYUV422ToRGB(
+  imageConvertUYVYToRGB(
     new_data, const_cast<uint8_t *>(data_ptr),
     0, height_, width_, stride_);
 
@@ -424,13 +396,13 @@ ROSImageTexture::convertYUV422ToRGBData(const uint8_t * data_ptr, size_t data_si
 }
 
 ImageData
-ROSImageTexture::convertYUV422_YUY2ToRGBData(const uint8_t * data_ptr, size_t data_size_in_bytes)
+ROSImageTexture::convertYUYVToRGBData(const uint8_t * data_ptr, size_t data_size_in_bytes)
 {
   size_t new_size_in_bytes = data_size_in_bytes * 3 / 2;
 
   uint8_t * new_data = new uint8_t[new_size_in_bytes];
 
-  imageConvertYUV422_YUY2ToRGB(
+  imageConvertYUYVToRGB(
     new_data, const_cast<uint8_t *>(data_ptr),
     0, height_, width_, stride_);
 
@@ -473,10 +445,10 @@ ROSImageTexture::setFormatAndNormalizeDataIfNecessary(
     return ImageData(Ogre::PF_BYTE_L, data_ptr, data_size_in_bytes, false);
   } else if (encoding == sensor_msgs::image_encodings::TYPE_32FC1) {
     return convertTo8bit<float>(data_ptr, data_size_in_bytes);
-  } else if (encoding == sensor_msgs::image_encodings::YUV422) {
-    return convertYUV422ToRGBData(data_ptr, data_size_in_bytes);
-  } else if (encoding == sensor_msgs::image_encodings::YUV422_YUY2) {
-    return convertYUV422_YUY2ToRGBData(data_ptr, data_size_in_bytes);
+  } else if (encoding == sensor_msgs::image_encodings::UYVY) {
+    return convertUYVYToRGBData(data_ptr, data_size_in_bytes);
+  } else if (encoding == sensor_msgs::image_encodings::YUYV) {
+    return convertYUYVToRGBData(data_ptr, data_size_in_bytes);
   } else {
     throw UnsupportedImageEncoding(encoding);
   }
--- rviz_ogre_vendor/CHANGELOG.rst
+++ rviz_ogre_vendor/CHANGELOG.rst
@@ -2,35 +2,45 @@
 Changelog for package rviz_ogre_vendor
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-14.1.7 (2025-01-14)
+14.4.1 (2025-01-15)
 -------------------
 
-14.1.6 (2024-12-18)
+14.4.0 (2024-12-20)
 -------------------
 
-14.1.5 (2024-09-06)
+14.3.3 (2024-11-25)
 -------------------
 
-14.1.4 (2024-08-19)
+14.3.2 (2024-11-20)
 -------------------
 
-14.1.3 (2024-07-19)
+14.3.1 (2024-10-11)
 -------------------
 
-14.1.2 (2024-06-27)
+14.3.0 (2024-10-03)
 -------------------
-* Fix flags for both clang and gcc. (`#1219 <https://github.com/ros2/rviz/issues/1219>`_) (`#1223 <https://github.com/ros2/rviz/issues/1223>`_)
-  In particular, make sure that a clang-only flag
-  (-Wno-implicit-const-int-float-conversion) is only
-  set for clang, and also add in another suppression
-  for g++ 13 (where there are false warnings for stringop-overflow).
-  (cherry picked from commit 0707355430b5a1c915c8a57961099155bb40cef8)
-  Co-authored-by: Chris Lalancette <clalancette@gmail.com>
-* Contributors: mergify[bot]
 
-14.1.1 (2024-05-13)
+14.2.6 (2024-08-28)
 -------------------
-* Update zlib into CMakeLists.txt (`#1128 <https://github.com/ros2/rviz/issues/1128>`_) (`#1195 <https://github.com/ros2/rviz/issues/1195>`_)
+
+14.2.5 (2024-07-29)
+-------------------
+
+14.2.4 (2024-07-19)
+-------------------
+
+14.2.3 (2024-06-25)
+-------------------
+* Fix flags for both clang and gcc. (`#1219 <https://github.com/ros2/rviz/issues/1219>`_)
+* Update freetype lib (`#1216 <https://github.com/ros2/rviz/issues/1216>`_)
+* Contributors: Chris Lalancette, mosfet80
+
+14.2.2 (2024-06-17)
+-------------------
+
+14.2.1 (2024-05-24)
+-------------------
+* Update zlib into CMakeLists.txt (`#1128 <https://github.com/ros2/rviz/issues/1128>`_)
   Changes in 1.3 (18 Aug 2023)
   - Remove K&R function definitions and zlib2ansi
   - Fix bug in deflateBound() for level 0 and memLevel 9
@@ -49,9 +59,10 @@ Changelog for package rviz_ogre_vendor
   - Various documentation improvements
   - Various spelling and typo corrections
   Co-authored-by: Chris Lalancette <clalancette@gmail.com>
-  (cherry picked from commit 32eb8b9404927883247e868ab0c7d62b80df2ed1)
-  Co-authored-by: mosfet80 <realeandrea@yahoo.it>
-* Contributors: mergify[bot]
+* Contributors: mosfet80
+
+14.2.0 (2024-04-26)
+-------------------
 
 14.1.0 (2024-04-16)
 -------------------
