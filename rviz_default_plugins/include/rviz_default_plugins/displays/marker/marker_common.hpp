6a6af0d8bf216501ee8d3e96780768671344de04
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-05-05 13:22:54 +0200

    Rebase from 'upstream'

--- rviz_default_plugins/include/rviz_default_plugins/displays/marker/marker_common.hpp
+++ rviz_default_plugins/include/rviz_default_plugins/displays/marker/marker_common.hpp
@@ -126,6 +126,8 @@ public:
   resource_retriever::Retriever * getResourceRetriever();
 
 private:
+  /** @brief Change the visibility for all markers in the given namespace. */
+  void setVisibilityForMarkersInNamespace(const std::string & ns, bool visible);
   /** @brief Delete all the markers within the given namespace. */
   void deleteMarkersInNamespace(const std::string & ns);
 
@@ -167,7 +169,7 @@ private:
   typedef QHash<QString, MarkerNamespace *> M_Namespace;
   M_Namespace namespaces_;
 
-  rviz_common::properties::Property * namespaces_category_;
+  rviz_common::properties::BoolProperty * namespaces_category_;
 
   typedef std::map<QString, bool> M_EnabledState;
   M_EnabledState namespace_config_enabled_state_;
@@ -179,6 +181,8 @@ private:
   Ogre::SceneNode * scene_node_;
 
   resource_retriever::Retriever retriever_;
+
+  bool all_namespaces_enabled_ = true;
 
   friend class MarkerNamespace;
 };
