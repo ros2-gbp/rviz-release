8220d4735318e34473e5b8c9dedd52c411bb10d3
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-09-04 15:49:40 +0200

    Imported upstream version '15.1.9' of 'upstream'

--- rviz_default_plugins/src/rviz_default_plugins/displays/map/palette_builder.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/displays/map/palette_builder.cpp
@@ -1,31 +1,32 @@
-/*
- * Copyright (c) 2018, Bosch Software Innovations GmbH.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the copyright holder nor the names of its contributors
- *       may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (c) 2018, Bosch Software Innovations GmbH.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 
 #include "rviz_default_plugins/displays/map/palette_builder.hpp"
 
@@ -84,13 +85,22 @@ std::vector<unsigned char> PaletteBuilder::buildPalette()
   return palette_;
 }
 
-std::vector<unsigned char> makeMapPalette()
+std::vector<unsigned char> makeMapPalette(bool binary, int threshold)
 {
   auto palette_builder = std::make_shared<PaletteBuilder>();
-  for (unsigned char i = 0; i <= 100; i++) {
-    // Standard gray map palette values
-    unsigned char v = 255 - (255 * i) / 100;
-    palette_builder->setColorForValue(i, v, v, v, 255);
+  if (binary) {
+    for (unsigned char i = 0; i < threshold; i++) {
+      palette_builder->setColorForValue(i, 255, 255, 255, 255);
+    }
+    for (unsigned char i = threshold; i <= 100; i++) {
+      palette_builder->setColorForValue(i, 0, 0, 0, 255);
+    }
+  } else {
+    for (unsigned char i = 0; i <= 100; i++) {
+      // Standard gray map palette values
+      unsigned char v = 255 - (255 * i) / 100;
+      palette_builder->setColorForValue(i, v, v, v, 255);
+    }
   }
   return palette_builder->setColorForIllegalPositiveValues(0, 255, 0)
          ->setRedYellowColorsForIllegalNegativeValues()
@@ -98,16 +108,25 @@ std::vector<unsigned char> makeMapPalette()
          ->buildPalette();
 }
 
-std::vector<unsigned char> makeCostmapPalette()
+std::vector<unsigned char> makeCostmapPalette(bool binary, int threshold)
 {
   auto palette_builder = std::make_shared<PaletteBuilder>();
-  palette_builder->setColorForValue(0, 0, 0, 0, 0);
-  for (unsigned char i = 1; i <= 98; i++) {
-    unsigned char v = (255 * i) / 100;
-    palette_builder->setColorForValue(i, v, 0, 255 - v, 255);
+  if (binary) {
+    for (unsigned char i = 0; i < threshold; i++) {
+      palette_builder->setColorForValue(i, 0, 0, 255, 255);
+    }
+    for (unsigned char i = threshold; i <= 100; i++) {
+      palette_builder->setColorForValue(i, 255, 0, 0, 255);
+    }
+  } else {
+    palette_builder->setColorForValue(0, 0, 0, 0, 0);
+    for (unsigned char i = 1; i <= 98; i++) {
+      unsigned char v = (255 * i) / 100;
+      palette_builder->setColorForValue(i, v, 0, 255 - v, 255);
+    }
+    palette_builder->setColorForValue(99, 0, 255, 255, 255);  // obstacle values in cyan
+    palette_builder->setColorForValue(100, 255, 0, 255, 255);  // lethal obstacle values in purple
   }
-  palette_builder->setColorForValue(99, 0, 255, 255, 255);  // obstacle values in cyan
-  palette_builder->setColorForValue(100, 255, 0, 255, 255);  // lethal obstacle values in purple
 
   return palette_builder->setColorForIllegalPositiveValues(0, 255, 0)
          ->setRedYellowColorsForIllegalNegativeValues()
@@ -115,11 +134,20 @@ std::vector<unsigned char> makeCostmapPalette()
          ->buildPalette();
 }
 
-std::vector<unsigned char> makeRawPalette()
+std::vector<unsigned char> makeRawPalette(bool binary, int threshold)
 {
   auto palette_builder = std::make_shared<PaletteBuilder>();
-  for (int i = 0; i < 256; i++) {
-    palette_builder->setColorForValue(i, i, i, i, 255);
+  if (binary) {
+    for (unsigned char i = 0; i < threshold; i++) {
+      palette_builder->setColorForValue(i, 0, 0, 0, 255);
+    }
+    for (int i = threshold; i < 256; i++) {
+      palette_builder->setColorForValue(i, 255, 255, 255, 255);
+    }
+  } else {
+    for (int i = 0; i < 256; i++) {
+      palette_builder->setColorForValue(i, i, i, i, 255);
+    }
   }
   return palette_builder->buildPalette();
 }
