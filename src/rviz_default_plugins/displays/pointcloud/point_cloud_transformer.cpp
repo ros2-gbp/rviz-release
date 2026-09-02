956c5be6fe7071ff74157a13ab0cc97f7c257701
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-09-02 09:44:51 +0200

    Rebase from 'release/rolling/rviz_default_plugins'

--- src/rviz_default_plugins/displays/pointcloud/point_cloud_transformer.cpp
+++ src/rviz_default_plugins/displays/pointcloud/point_cloud_transformer.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, Bosch Software Innovations GmbH.
+// Copyright (c) 2026, Open Source Robotics Foundation, Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -27,62 +27,35 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include "rviz_default_plugins/displays/pointcloud/point_cloud_transformer.hpp"
 
-#ifndef RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
-#define RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
+#include <QList>
 
-#include <chrono>
-#include <string>
-#include <vector>
+#include "rviz_common/properties/property.hpp"
 
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include "tf2/LinearMath/Quaternion.hpp"
-#include "rclcpp/node.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include "std_msgs/msg/header.hpp"
-
-#include "../pointcloud_messages.hpp"
-
-using namespace std::chrono_literals;  // NOLINT
-
-namespace nodes
+namespace rviz_default_plugins
 {
 
-geometry_msgs::msg::Point32 createPoint(float x, float y, float z)
-{
-  geometry_msgs::msg::Point32 point;
-  point.x = x;
-  point.y = y;
-  point.z = z;
+void PointCloudTransformer::init()
+{}
 
-  return point;
+uint8_t PointCloudTransformer::score(const sensor_msgs::msg::PointCloud2::ConstSharedPtr & cloud)
+{
+  (void) cloud;
+  return 0;
 }
 
-class PointCloud2Publisher : public rclcpp::Node
+void PointCloudTransformer::createProperties(
+  rviz_common::properties::Property * parent_property,
+  uint32_t mask,
+  QList<rviz_common::properties::Property *> & out_props)
 {
-public:
-  PointCloud2Publisher()
-  : Node("pointcloud2_publisher"),
-    timer_(nullptr),
-    publisher_(nullptr)
-  {
-    publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("pointcloud2", 10);
-    timer_ = this->create_wall_timer(500ms, std::bind(&PointCloud2Publisher::timer_callback, this));
-  }
+  (void) parent_property;
+  (void) mask;
+  (void) out_props;
+}
 
-private:
-  void timer_callback()
-  {
-    auto message = rviz_default_plugins::createPointCloud2WithPoints({{0, 0, 0}});
-    message->header.frame_id = "pointcloud2_frame";
+void PointCloudTransformer::hideUnusedProperties()
+{}
 
-    publisher_->publish(*message);
-  }
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
-};
-
-}  // namespace nodes
-
-#endif  // RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
+}  // namespace rviz_default_plugins
