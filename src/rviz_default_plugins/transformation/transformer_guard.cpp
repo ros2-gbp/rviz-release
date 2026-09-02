ed59fd96af4c0b0f827cfd52a503408d8e62558f
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-09-02 09:46:05 +0200

    Rebase from 'release/rolling/rviz_default_plugins'

--- src/rviz_default_plugins/transformation/transformer_guard.cpp
+++ src/rviz_default_plugins/transformation/transformer_guard.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, Bosch Software Innovations GmbH.
+// Copyright (c) 2026, Open Source Robotics Foundation, Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -27,62 +27,45 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include "rviz_default_plugins/transformation/transformer_guard.hpp"
 
-#ifndef RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
-#define RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
-
-#include <chrono>
 #include <string>
-#include <vector>
 
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include "tf2/LinearMath/Quaternion.hpp"
-#include "rclcpp/node.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include "std_msgs/msg/header.hpp"
+#include "rviz_common/display.hpp"
+#include "rviz_common/display_context.hpp"
+#include "rviz_common/transformation/transformation_manager.hpp"
 
-#include "../pointcloud_messages.hpp"
-
-using namespace std::chrono_literals;  // NOLINT
-
-namespace nodes
+namespace rviz_default_plugins
+{
+namespace transformation
 {
 
-geometry_msgs::msg::Point32 createPoint(float x, float y, float z)
-{
-  geometry_msgs::msg::Point32 point;
-  point.x = x;
-  point.y = y;
-  point.z = z;
+_TransformerGuard::_TransformerGuard(
+  rviz_common::Display * display,
+  const std::string & transformer_name)
+: display_(display),
+  allowed_transformer_name_(transformer_name),
+  using_allowed_transformer_(true),
+  display_disabled_by_user_(false),
+  context_(nullptr)
+{}
 
-  return point;
+void
+_TransformerGuard::initialize(rviz_common::DisplayContext * context)
+{
+  context_ = context;
+  connect(
+    context_->getTransformationManager(),
+    SIGNAL(transformerChanged(std::shared_ptr<rviz_common::transformation::FrameTransformer>)),
+    this,
+    SLOT(transformerChanged(std::shared_ptr<rviz_common::transformation::FrameTransformer>)));
+  connect(display_, SIGNAL(changed()), this, SLOT(displayEnabledChanged()));
+
+  if (!checkTransformer()) {
+    using_allowed_transformer_ = false;
+    Q_EMIT (display_->changed());
+  }
 }
 
-class PointCloud2Publisher : public rclcpp::Node
-{
-public:
-  PointCloud2Publisher()
-  : Node("pointcloud2_publisher"),
-    timer_(nullptr),
-    publisher_(nullptr)
-  {
-    publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("pointcloud2", 10);
-    timer_ = this->create_wall_timer(500ms, std::bind(&PointCloud2Publisher::timer_callback, this));
-  }
-
-private:
-  void timer_callback()
-  {
-    auto message = rviz_default_plugins::createPointCloud2WithPoints({{0, 0, 0}});
-    message->header.frame_id = "pointcloud2_frame";
-
-    publisher_->publish(*message);
-  }
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
-};
-
-}  // namespace nodes
-
-#endif  // RVIZ_DEFAULT_PLUGINS__PUBLISHERS__POINT_CLOUD2_PUBLISHER_HPP_
+}  // namespace transformation
+}  // namespace rviz_default_plugins
